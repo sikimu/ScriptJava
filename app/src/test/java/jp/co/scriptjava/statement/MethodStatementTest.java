7f53e09ab67c0bb5b1fff09fca3abc61c89b661b
sikimu <sikimu.pg@gmail.com>
2023-07-27 22:51:50 +0900

    無駄削除

--- app/src/test/java/jp/co/scriptjava/statement/MethodStatementTest.java
+++ app/src/test/java/jp/co/scriptjava/statement/MethodStatementTest.java
@@ -1,6 +1,5 @@
 package jp.co.scriptjava.statement;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;

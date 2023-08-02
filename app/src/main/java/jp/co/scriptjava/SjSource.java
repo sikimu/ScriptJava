ca4a42afed0d3d2e0691a8d6a5fb78fe2651139f
sikimu <sikimu.pg@gmail.com>
2023-08-02 22:53:29 +0900

    これはさすがにテストいらないか？

--- app/src/main/java/jp/co/scriptjava/SjSource.java
+++ app/src/main/java/jp/co/scriptjava/SjSource.java
@@ -2,11 +2,38 @@ package jp.co.scriptjava;
 
 import java.util.List;
 
+import jp.co.scriptjava.statement.ClassStatement;
+import jp.co.scriptjava.statement.ImportStatement;
+import jp.co.scriptjava.statement.PackageStatement;
 import jp.co.scriptjava.statement.Statement;
 
 public class SjSource {
 
+    /**
+     * コメントマップ
+     */
+    final public CommentMap commentMap;
+
+    /**
+     * パッケージ
+     */
+    final public Statement packageStatement;
+
+    /**
+     * インポートリスト
+     */
+    final public List<Statement> importStatementList;
+
+    /**
+     * クラスリスト
+     */
+    final public List<Statement> classStatement;
+
     public SjSource(List<Statement> statements, CommentMap commentMap) {
+        this.commentMap = commentMap;
+        this.packageStatement = statements.stream().filter(s -> s instanceof PackageStatement).findFirst().orElse(null);
+        this.importStatementList = statements.stream().filter(s -> s instanceof ImportStatement).toList();
+        this.classStatement = statements.stream().filter(s -> s instanceof ClassStatement).toList();
     }
     
 }
---
+++ app/src/test/java/jp/co/scriptjava/SampleCode.java
@@ -0,0 +1,17 @@
+package jp.co.scriptjava;
+
+public class SampleCode {
+    
+    /**
+     * a+bを計算する
+     * @param a
+     * @param b
+     * @return
+     */
+    public int add(int a, int b){
+        
+        int c = a + b;
+
+        return c;
+    }
+}

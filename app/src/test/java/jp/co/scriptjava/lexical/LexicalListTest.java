77882087140a749aacf351b8fd275132dc1b6c70
sikimu <sikimu.pg@gmail.com>
2023-08-14 14:24:05 +0900

    いらなかった

--- app/src/test/java/jp/co/scriptjava/lexical/LexicalListTest.java
+++ app/src/test/java/jp/co/scriptjava/lexical/LexicalListTest.java
@@ -2,7 +2,6 @@ package jp.co.scriptjava.lexical;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.List;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
